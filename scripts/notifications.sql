56c714bfefd341f3db5c63647556e8b0b513fc38
Kamesh Sampath <kamesh.sampath@hotmail.com>
2024-12-11 13:41:40 +0530

    (fix): remove slack app notification

--- scripts/notifications.sql
+++ scripts/notifications.sql
@@ -9,13 +9,13 @@ desc secret kamesh_demos.alerts_and_notification.slack_demomate_webhook;
 -- create the notification integration
 
 -- Send message to App
-create or replace notification integration kameshs_slack_demomate
-  type = WEBHOOK
-  enabled = true
-  webhook_url = 'https://hooks.slack.com/services/SNOWFLAKE_WEBHOOK_SECRET'
-  webhook_secret = kamesh_demos.alerts_and_notification.slack_demomate_webhook
-  webhook_body_template='SNOWFLAKE_WEBHOOK_MESSAGE'
-  webhook_headers=('Content-Type'='application/json');
+CREATE OR REPLACE NOTIFICATION INTEGRATION kameshs_slack_demomate
+  TYPE = WEBHOOK
+  ENABLED = true
+  WEBHOOK_URL = 'https://hooks.slack.com/services/SNOWFLAKE_WEBHOOK_SECRET'
+  WEBHOOK_SECRET = kamesh_demos.alerts_and_notification.slack_demomate_webhook
+  WEBHOOK_BODY_TEMPLATE='SNOWFLAKE_WEBHOOK_MESSAGE'
+  WEBHOOK_HEADERS=('Content-Type'='application/json');
 
 -- send to channel 
 CREATE OR REPLACE NOTIFICATION INTEGRATION kameshs_slack_alerts_notifications
